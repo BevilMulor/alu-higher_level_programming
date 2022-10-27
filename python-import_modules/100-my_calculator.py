9919908f728e135172bd19880d0f94cdcf222c1d
root <b.mulor@alustudent>
2022-10-27 10:35:49 -0700

    Executable files given

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-from sys import argv
 from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+
+argc = len(argv)
 
 if __name__ == "__main__":
-    args = len(argv)
 
-    if args != 4:
+    if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-        a = int(argv[1])
-        op = argv[2]
-        b = int(argv[3])
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-        if op == "+":
-            num = add(a, b)
-        elif op == "-":
-            num = sub(a, b)
-        elif op == "*":
-            num = mul(a, b)
-        elif op == "/":
-            num = div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        print("{:d} {} {:d} = {:d}".format(a, op, b, num))
+    a = int(argv[1])
+    b = int(argv[3])
+    print(f"{a} {argv[2]} {b} = {operators[argv[2]](a, b)}")
