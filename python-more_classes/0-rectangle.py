83628b0e34550d25aa1fbd975274b898c6b0347c
root <b.mulor@alustudent>
2022-11-11 11:59:12 -0800

    python more classes

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# empty class Rectangle defininga rectangle
+"""
+    define a class 'Rectangle'
+"""
+
+
+class Rectangle:
+"""
+    rectangle
+"""
+pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Readme file for python_more_classes
