073cf4cd058fca9d7b63b8cac465b8738ed29fde
root <b.mulor@alustudent>
2022-11-11 03:44:58 -0800

    python-exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+# function printing x elements of a list
+
 
 def safe_print_list(my_list=[], x=0):
-
-    num = 0
-    for n in range(x):
+    ret = 0
+    for i in range(x):
         try:
-            print("{}".format(my_list[n]), end="")
-            num += 1
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
-        print("")
-        return (num)
+    print("")
+    return (ret)
