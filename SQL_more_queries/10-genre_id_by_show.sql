9f3e6ee2646295248883f7aede417252ab1756ba
root <b.mulor@alustudent>
2022-11-23 13:51:32 -0800

    SQL_more_queries

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,4 @@
--- script that lists all the cities of California that can be found in the database 'hbtn_0d_usa'
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
+-- script that lists all shows contained in 'hbtn_0d_tvshows' that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
