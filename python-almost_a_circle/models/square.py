a8dc69a19e8bcaf8fc4f3ed347eaeade309ee73b
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2022-12-07 22:06:54 +0000

    almost circle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,81 +1,54 @@
 #!/usr/bin/python3
-"""Defines a square class."""
+# class Square inheriting from Rectangle
+"""A rectangle module"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """ A Square class """
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
-        """
+        """Initialize a square instance"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns the string representation of a square"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Get/set the size of the Square."""
+        """Getter for size"""
         return self.width
 
     @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
+    def size(self, size):
+        """Setter for size"""
+        self.width = size
+        self.height = size
 
     def update(self, *args, **kwargs):
-        """Update the Square.
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
+        """Updates the attributes of a square"""
+        if args is not None and len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
                     self.size = arg
-                elif a == 2:
+                elif i == 2:
                     self.x = arg
-                elif a == 3:
+                elif i == 3:
                     self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y
-                                                 self.width)
+        """Returns the dictionary representation of a square"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
