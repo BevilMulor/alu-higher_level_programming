477f020f258028762e831d7b35132ec682f739a7
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2023-02-07 12:20:09 +0000

    Objects scopes

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
-    for (const i of list) {
-      if (i === searchElement) {
-        count++;
-      }
-    }
-    return count;
+    return list.filter((n) => n === searchElement).length;
   };
   
--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
-exports.esrever = function (list) {
-    const rev = [];
-    for (let i = 0; i < list.length; i++) {
-      rev.unshift(list[i]);
+exports.esrever = function (list = []) {
+    const newList = [];
+    for (let i = list.length; i >= 0; i--) {
+      newList.push(list[i]);
     }
-    return rev;
+    newList.shift();
+    return newList;
   };
-  
