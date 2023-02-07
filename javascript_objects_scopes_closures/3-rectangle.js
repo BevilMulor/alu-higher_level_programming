85a062cc1f21ed172910a178870565b4c0f93a7a
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2023-02-07 12:41:23 +0000

    Objects scope

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
-
 module.exports = class Rectangle {
     constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
     }
   
     print () {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('X');
-        }
-        if (i < this.height) { process.stdout.write('\n'); }
-      }
+      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
     }
   };
   
